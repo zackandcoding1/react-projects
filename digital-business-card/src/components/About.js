d75e13e11461bf4c74c0a54375498df4f7391821
Isaac Gomes <isaacnbgomes@gmail.com>
2024-02-29 12:53:21 -0300

    about description changes

--- digital-business-card/src/components/About.js
+++ digital-business-card/src/components/About.js
@@ -2,7 +2,7 @@ function About() {
     return(
         <div className="about--card">
             <h4>About</h4>
-            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p>I'm a Computer Science student and I'm currently improving my JavaScript/React skills.</p>
         </div>
     )
 }

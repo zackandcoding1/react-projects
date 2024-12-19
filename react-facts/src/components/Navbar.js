428e8343c3ed2ee7e7a3a7ebe171bfc1574088da
zackandcoding1 <isaacnbgomes@gmail.com>
2024-12-19 17:12:06 -0300

    started reactfacts project to practice

---
+++ react-facts/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return(
+        <nav className="navbar">
+            <img className="react-logo" src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="react logo"/>
+            <span className="react-title">ReactFacts</span>
+        </nav>
+    )
+}
